125448018a43c2ab0981f9bb9eaedac5dbde9415
Mohammed Tahleel <mtahleel11@gmail.com>
2024-06-26 00:36:38 +0300

    made database connection changes

--- db/config.js
+++ db/config.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
-mongoose.connect("mongodb://localhost:27017/e-commerce");
+const DB = process.env.DATABASE;
+mongoose.connect(DB);
 
-// const DB = process.env.DATABASE;
 // mongoose
 //   .connect(DB)
 //   .then(() => console.log("Database connected"))
